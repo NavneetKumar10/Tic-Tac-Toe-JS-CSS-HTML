bfbced2e7a9b733c8b31fdca845458061e4894e9
NavneetKumar10 <64538513+NavneetKumar10@users.noreply.github.com>
2024-04-25 22:56:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
 let turnO = true; // playerX, playerO
+let timer; // Variable to hold the timer
 
 const winPatterns = [
     [0, 1, 2],
@@ -23,18 +24,36 @@ const resetGame = () => {
     msgContainer.classList.add("hide");
 };
 
+const startTimer = () => {
+    timer = setTimeout(() => {
+        switchTurn();
+    }, 4000); // 4 seconds delay
+};
+
+const resetTimer = () => {
+    clearTimeout(timer);
+    startTimer();
+};
+
+const switchTurn = () => {
+    turnO = !turnO;
+    resetTimer();
+};
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        if (turnO){
-            box.innerText ="O";
-            turnO=false;
-        } else {
-            box.innerText = "X";
-            turnO=true;
+        if (!box.disabled) { // Only proceed if the box is not disabled
+            if (turnO){
+                box.innerText = "O";
+                turnO = false;
+            } else {
+                box.innerText = "X";
+                turnO = true;
+            }
+            box.disabled = true;
+            resetTimer(); // Reset the timer after each move
+            checkWinner();
         }
-        box.disabled = true;
-
-        checkWinner();
     });
 });
 
@@ -42,31 +61,29 @@ const disableBoxes = () => {
     for (let box of boxes) {
         box.disabled = true;
     }
-}
+};
 
 const enableBoxes = () => {
     for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
-}
+};
 
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
-}
+};
 
-const checkWinner = () =>{
+const checkWinner = () => {
     for (let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
 
-        let pos1Val=boxes[pattern[0]].innerText;
-        let pos2Val=boxes[pattern[1]].innerText;
-        let pos3Val=boxes[pattern[2]].innerText;
-
-        if (pos1Val!="" && pos2Val!="" && pos3Val !=""){
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== ""){
             if (pos1Val === pos2Val && pos2Val === pos3Val){
-                
                 showWinner(pos1Val);
             }
         }
@@ -75,3 +92,6 @@ const checkWinner = () =>{
 
 newGameBtn.addEventListener("click" , resetGame);
 resetBtn.addEventListener("click", resetGame);
+
+// Start the timer when the page loads
+startTimer();
